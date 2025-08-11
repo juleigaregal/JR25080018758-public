8a1fc444f80916b77589a4fc582eebcdd2273f08
Jules Regal <48181066+juleigaregal@users.noreply.github.com>
2025-08-11 08:45:29 +0200

    Add files via upload

---
+++ Level 1 - Python for Data Science/M01T02 – Your First Computer Program/hello_world.py
@@ -0,0 +1,15 @@
+# Understand what the algorithm needs to do
+# The code needs to have a user input their name and age separately
+# Code needs to output "Hello, World!"
+
+#Get user to input name
+#print out the name
+#Get user to input age
+#print out the age
+#print "Hello, World!"
+
+name = input("Enter your name: ")
+print(name)
+age = input("Enter your age: ")
+print (age)
+print("Hello", " World!")
