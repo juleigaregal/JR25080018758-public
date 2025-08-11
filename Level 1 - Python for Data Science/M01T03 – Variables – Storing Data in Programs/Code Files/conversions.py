7292e150c813ef5e76f4aeed998726b146771674
Jules Regal <48181066+juleigaregal@users.noreply.github.com>
2025-08-11 16:24:20 +0200

    Add files via upload

---
+++ Level 1 - Python for Data Science/M01T03 – Variables – Storing Data in Programs/Code Files/conversions.py
@@ -0,0 +1,19 @@
+#code to convert variables to different data types
+
+#assign variables
+num1 = 99.23
+num2 = 23
+num3 = 150
+string = "100"
+
+#perform conversions
+num1_integer = int(num1)
+num2_float = float(num2)
+num3_string = str(num3)
+string_to_int = int(string)
+
+#print converted variables
+print(num1_integer)
+print(num2_float)
+print(num3_string)
+print(string_to_int)
