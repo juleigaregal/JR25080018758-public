711f0c6b8ef928e472395870101e76373c948f1b
Jules Regal <48181066+juleigaregal@users.noreply.github.com>
2025-08-11 16:17:20 +0200

    Add files via upload

---
+++ Level 1 - Python for Data Science/M01T03 – Variables – Storing Data in Programs/Code Files/details.py
@@ -0,0 +1,10 @@
+#Code is used to get data from a user
+#Name, Age, house number and streetname
+#the code my print out one line with all the inputs as a sentence using f-strings.
+
+Name = input("Please enter your name:")
+Age = input("Please enter your age:")
+House_number = input("Please enter your house number:")
+street_name = input("Please enter your street name:")
+
+print(f"My name is {Name} and I am {Age} years old, and I live at {House_number} {street_name}.") #prints out inputs using f-strings
