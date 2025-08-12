ef2d137d53c1d217a2529d9038f54ccb733b74b5
Jules Regal <48181066+juleigaregal@users.noreply.github.com>
2025-08-12 18:14:25 +0200

    Add files via upload

---
+++ Level 1 - Python for Data Science/M01T03 – Variables – Storing Data in Programs/Code Files/integermath.py
@@ -0,0 +1,12 @@
+int1 = int(input("Please enter an integer:"))
+int2 = int(input("Please enter a second integer:"))
+int3 = int(input("Please enter a third integer:"))
+
+print(int1)
+print(int2)
+print(int3)
+
+print(int1+int2+int3)
+print(int1 - int2)
+print(int3*int1)
+print((int1+int2+int3)/int3)
