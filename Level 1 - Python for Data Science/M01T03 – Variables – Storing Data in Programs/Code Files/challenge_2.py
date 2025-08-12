1ce10ff355c01c810861d9cac303a73782383de3
Jules Regal <48181066+juleigaregal@users.noreply.github.com>
2025-08-12 18:37:24 +0200

    Add files via upload

---
+++ Level 1 - Python for Data Science/M01T03 – Variables – Storing Data in Programs/Code Files/challenge_2.py
@@ -0,0 +1,5 @@
+string_fav = input("Please enter your favourite restaurant:")
+int_fav = int(input("Please enter your favourite number:"))
+print(string_fav)
+print(int_fav)
+print(int(string_fav)) #you get an error message because it can't convert text to an integer
