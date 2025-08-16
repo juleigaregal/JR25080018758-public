6cff302edd6d6b0375192db663686c55d75eaa3f
Jules Regal <48181066+juleigaregal@users.noreply.github.com>
2025-08-16 14:08:29 +0200

    Add files via upload

--- Level 1 - Python for Data Science/M02T03 – Defensive Programming – Error Handling/Code Files/Task files/errors2.py
+++ Level 1 - Python for Data Science/M02T03 – Defensive Programming – Error Handling/Code Files/Task files/errors2.py
@@ -1,12 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 16 14:04:06 2025
+
+@author: juleigar
+"""
+
 # This example program is meant to demonstrate errors.
  
 # There are some errors in this program. Run the program, look at the error messages, and find and fix the errors.
 
-animal = Lion
+animal = "Lion" #syntax error missing qoutation marks
 animal_type = "cub"
 number_of_teeth = 16
 
-full_spec = "This is a {animal}. It is a {number_of_teeth} and it has {animal_type} teeth"
+full_spec = (f"This is a {animal}. It is a {animal_type} and it has {number_of_teeth} teeth") #syntax error and logical error the variables in wrong order, need to use {} to add the avariable
 
-print full_spec
-
+print(full_spec) #synax error missing the brackets
