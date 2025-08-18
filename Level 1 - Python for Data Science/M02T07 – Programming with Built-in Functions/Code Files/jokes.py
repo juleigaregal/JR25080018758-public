d6bec42ba822d83f5e1d06917c1e313b9c080f11
Jules Regal <48181066+juleigaregal@users.noreply.github.com>
2025-08-18 15:06:24 +0200

    Update jokes.py

--- Level 1 - Python for Data Science/M02T07 – Programming with Built-in Functions/Code Files/jokes.py
+++ Level 1 - Python for Data Science/M02T07 – Programming with Built-in Functions/Code Files/jokes.py
@@ -5,13 +5,14 @@ Created on Mon Aug 18 14:49:49 2025
 @author: juleigar
 """
 import random 
-
-
-
+# Randomly select jokes from a list using random built in function
+# obtained jokes from https://www.stylecraze.com/
 jokes = ["What do dentists call their X-rays? Tooth pics", "When your teacher asks “Where’s your homework?” It took a sick day. It had too many problems.", 
           "You can’t trust atoms. They make up everything!", "Why was 6 afraid of 7? Because 7 ate 9!",
           "Why won’t it hurt if you hit your friend with a 2-liter of soda? Because it’s a soft drink!"
           ,"The broom was late today because it overswept last night!",
           "What caused the bicycle to collapse? Because it was two tired."]
 
+#print jokes from list at random
 print(random.choice(jokes))   
+
