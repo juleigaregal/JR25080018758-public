0353de72246385533a10d66e64a06fada49cf0a4
Jules Regal <48181066+juleigaregal@users.noreply.github.com>
2025-08-18 13:25:05 +0200

    Add files via upload

---
+++ Level 1 - Python for Data Science/M02T07 – Programming with Built-in Functions/Code Files/float_manipulation.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 18 12:59:07 2025
+
+@author: juleigar
+"""
+
+import math 
+from statistics import mean, median
+
+stored_numbers = []
+total_numbers = 0
+for i in range(0,5):
+    items = float(input("Enter a float number:"))
+    stored_numbers.append(items)
+    total_numbers+=items
+    
+
+print(f"The total is {total_numbers}")
+print(f"The minimum is {min(stored_numbers)}")
+print(f"The maximum is {max(stored_numbers)}")
+print(f"The mean is {mean(stored_numbers)}")
+print(f"The median is {median(stored_numbers)}")
